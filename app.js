03b2e3c3a4f237e14cc5a75fafaca8cee2645b24
Jeff-Francis Ofoaro <jeff-francis.ofoaro@buildingminds.com>
2021-03-01 17:39:02 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+import express from "express";
+import CSVToJSON from "csvtojson";
+import { join } from "path";
+import fs from "fs-extra";
+
+import masterDataIdArray from "./formatedMasterData";
+
+const app = express();
+const PORT = 3456 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("hello I am here ");
+});
+
+const printCSVWithoutMatch = async (inputCsv, outputCsv, masterdataArray) => {
+  await CSVToJSON()
+    .fromFile(inputCsv)
+    .then(jsonedCSV => {
+      const csvArr = jsonedCSV.map(x => x.sitesBASF);
+      console.log("jsoned CSV", csvArr);
+
+      const filteredArrWithoutHeader = masterdataArray.filter(item => !csvArr.includes(item));
+
+      const filteredArr = ["IDs not Mapped", ...filteredArrWithoutHeader];
+      // eslint-disable-next-line no-useless-escape
+      const dataToSave = filteredArr.map(item => JSON.stringify(item, null, 1).replace(/\"/gi, "")).join("\n");
+
+      // console.log("csv pattern", dataToSave);
+      fs.writeFile(outputCsv, dataToSave, err => {
+        if (err) {
+          console.log(err);
+        }
+        console.log("the file was saved");
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+console.log("The length of Master Data is = ", masterDataIdArray.length);
+
+const outputFile = join(__dirname, "/results.csv");
+
+const csvFile = join(__dirname, "/sites.csv");
+
+printCSVWithoutMatch(csvFile, outputFile, masterDataIdArray);
+
+app.listen(PORT, err => {
+  if (!err) {
+    console.log(`Server is running on ${PORT}`);
+  } else {
+    console.log(err);
+  }
+});
---
+++ formatedMasterData.js
@@ -0,0 +1,5 @@
+import masterdata from "./masterdata.json";
+
+const response = masterdata.map(({ id }) => id); // .map(x=> x.id)
+
+export default response;
